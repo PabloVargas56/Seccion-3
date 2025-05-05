a0147d61c1247484b4465ca57cc38a278c359909
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-05-05 17:26:49 -0500

    Ejercicio 1 - Completado

---
+++ assets/js/ejercicio1.js
@@ -0,0 +1,11 @@
+let miNombre = "Pablo Tomas Rojas Vargas";
+let miEdad = 22;
+let meGustaProgramar = true;
+let sinValor  ;
+let variableNula = null;
+
+console.log({miNombre});
+console.log({miEdad});
+console.log({meGustaProgramar});
+console.log({sinValor});
+console.log({variableNula});
