9f05e97b0f627bcb3cdb0144230f5fc11f049767
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-05-05 17:37:41 -0500

    Funciones

---
+++ assets/js/funciones.js
@@ -0,0 +1,18 @@
+function saludar (nombre) {
+    return "Hola " + nombre 
+}
+
+console.log(saludar("Pablo Tomas Rojas Vargas"));
+
+let cuadradoDeUnNumero = function (a) {
+    return a * a;
+}
+
+console.log(cuadradoDeUnNumero(4));
+
+let numeroAleatorio = function () {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+console.log(numeroAleatorio());
+
